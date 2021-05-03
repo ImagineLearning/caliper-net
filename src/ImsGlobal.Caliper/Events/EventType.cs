76883f2965ea2db347dd0167fd1c25c8f250b61c
adamstaplesdev <adam.staples@imaginelearning.com>
2021-05-03 16:08:36 -0600

    Support for the ResourceManagementEvent type

--- src/ImsGlobal.Caliper/Events/EventType.cs
+++ src/ImsGlobal.Caliper/Events/EventType.cs
@@ -16,6 +16,7 @@ namespace ImsGlobal.Caliper.Events {
         public static readonly EventType Message = new EventType("MessageEvent");
         public static readonly EventType Navigation = new EventType( "NavigationEvent" );
 		public static readonly EventType Outcome = new EventType( "GradeEvent" );
+		public static readonly EventType ResourceManagement = new EventType ( "ResourceManagementEvent");
 		public static readonly EventType Session = new EventType( "SessionEvent" );
         public static readonly EventType Thread = new EventType("ThreadEvent");
 		public static readonly EventType ToolUse = new EventType("ToolUseEvent" );
---
+++ src/ImsGlobal.Caliper/Events/Media/ResourceManagementEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace ImsGlobal.Caliper.Events.Media {
+
+	/// <summary>
+	/// Event raised when an actor manages a digital resource.
+	/// </summary>
+	public class ResourceManagementEvent : Event {
+
+		public ResourceManagementEvent( string id, Action action ) 
+			:base( id ) {
+			this.Type = EventType.ResourceManagement;
+			this.Action = action;
+		}
+
+	}
+
+}
